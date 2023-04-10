adfbff48b4513110c23c5b4a3007c2fba0ecde55
tamanchichan <tamanchichan@gmail.com>
2023-04-10 10:23:14 -0500

    loading-screen added

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,10 @@
 'use strict';
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(getLocation, errorHandler);
-  navigator.geolocation.watchPosition(getLocation, errorHandler);
-} else {
-  alert('Geolocation is not supported by your browser');
-};
+const loadingScreen = document.querySelector('.loading-screen');
 
 function getLocation(position) {
+  loadingScreen.style.display = 'none';
+  
   const {latitude, longitude} = position.coords;
   
   mapboxgl.accessToken = 'pk.eyJ1IjoidGFtYW5jaGljaGFuIiwiYSI6ImNsZzEwamkwcjE0b20zcGxhdHk3b3Rnb2EifQ.WPkXz--D_pWcSZI2oMJBQQ';
@@ -23,5 +20,12 @@ function getLocation(position) {
 }
 
 function errorHandler() {
+  loadingScreen.style.animationPlayState = 'paused';
   alert('Unable to retrieve your location');
 }
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(getLocation, errorHandler);
+} else {
+  alert('Geolocation is not supported by your browser');
+};
