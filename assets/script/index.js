7ca42e83630e5e210ea792418ba0644f864faeda
tamanchichan <tamanchichan@gmail.com>
2023-04-10 09:30:23 -0500

    function updated

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,27 +2,24 @@
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(getLocation, errorHandler);
+  navigator.geolocation.watchPosition(getLocation, errorHandler);
 } else {
   alert('Geolocation is not supported by your browser');
 };
 
-let trackUser = navigator.geolocation.watchPosition(getLocation, errorHandler);
-
 function getLocation(position) {
-  // setInterval(() => {
-    const {latitude, longitude} = position.coords;
-  // }, 1000);
-    mapboxgl.accessToken = 'pk.eyJ1IjoidGFtYW5jaGljaGFuIiwiYSI6ImNsZzEwamkwcjE0b20zcGxhdHk3b3Rnb2EifQ.WPkXz--D_pWcSZI2oMJBQQ';
+  const {latitude, longitude} = position.coords;
   
-    let map = new mapboxgl.Map({
-      center: [longitude, latitude],
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 15
-    });
-    
-    new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
-  // }, 1000);
+  mapboxgl.accessToken = 'pk.eyJ1IjoidGFtYW5jaGljaGFuIiwiYSI6ImNsZzEwamkwcjE0b20zcGxhdHk3b3Rnb2EifQ.WPkXz--D_pWcSZI2oMJBQQ';
+  
+  let map = new mapboxgl.Map({
+    center: [longitude, latitude],
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 15
+  });
+  
+  new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
 }
 
 function errorHandler() {
